9a555e1fd3850e69bc024eed4ac1d096a9131e74
weitanai <987346185@qq.com>
2023-04-09 11:25:15 +0800

    feat: add auto publish script

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## [1.5.6](https://github.com/weitanai/file-exports/compare/v1.0.4...v1.5.6) (2023-04-09)
+
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -10,53 +10,134 @@ const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
 
 // 确认版本号
 const currentVersion = packageJson.version;
-enquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'version',
-      message: `请输入新版本号 (当前版本号 ${currentVersion}):`,
-      validate: (input) => {
-        if (!semver.valid(input)) {
-          return '版本号格式错误，请使用 Semantic Versioning 规范';
-        }
-        if (!semver.gt(input, currentVersion)) {
-          return '新版本号应该大于当前版本号';
-        }
-        return true;
-      },
-    },
-    {
-      type: 'input',
-      name: 'commitMessage',
-      message: '请输入提交信息:',
-    },
-  ])
-  .then(async (answers) => {
-    const { version, commitMessage } = answers;
 
-    // 更新版本号
-    packageJson.version = version;
-    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-
-    // 构建代码
-    await execa('npm', ['run', 'build']);
-
-    // 提交代码到GitHub
-    try {
-      await execa('git', ['add', '.']);
-      await execa('git', ['commit', '-m', commitMessage]);
-      await execa('git', ['push']);
-      console.log(chalk.green('代码已提交到GitHub'));
-    } catch (error) {
-      console.log(chalk.red('提交代码到GitHub失败，请手动提交'));
+const versionIncrements = [
+    'patch',
+    'minor',
+    'major'
+  ]
+  
+  const tags = [
+    'latest',
+    'next'
+  ]
+  
+  const inc = (i) => semver.inc(currentVersion, i)
+  const run = (bin, args, opts = {}) => execa(bin, args, { stdio: 'inherit', ...opts })
+  const step = (msg) => console.log(chalk.cyan(msg))
+  
+  async function main() {
+    let targetVersion
+  
+    const { release } = await enquirer.prompt({
+      type: 'select',
+      name: 'release',
+      message: 'Select release type',
+      choices: versionIncrements.map(i => `${i} (${inc(i)})`).concat(['custom'])
+    })
+  
+    if (release === 'custom') {
+      targetVersion = (await enquirer.prompt({
+        type: 'input',
+        name: 'version',
+        message: 'Input custom version',
+        initial: currentVersion
+      })).version
+    } else {
+      targetVersion = release.match(/\((.*)\)/)[1]
     }
-
-    // 发布到 npm
-    try {
-      await execa('npm', ['publish']);
-      console.log(chalk.green('已发布到npm'));
-    } catch (error) {
-      console.log(chalk.red('发布到npm失败，请手动发布'));
+  
+    if (!semver.valid(targetVersion)) {
+      throw new Error(`Invalid target version: ${targetVersion}`)
     }
-  });
+  
+    const { tag } = await enquirer.prompt({
+      type: 'select',
+      name: 'tag',
+      message: 'Select tag type',
+      choices: tags
+    })
+  
+    const { yes: tagOk } = await enquirer.prompt({
+      type: 'confirm',
+      name: 'yes',
+      message: `Releasing v${targetVersion} with the "${tag}" tag. Confirm?`
+    })
+  
+    if (!tagOk) {
+      return
+    }
+  
+    // Run tests before release.
+  
+    // Update the package version.
+    step('\nUpdating the package version...')
+    updatePackage(targetVersion)
+  
+    // Build the package.
+    step('\nBuilding the package...')
+    await run('npm', ['run', 'build'])
+  
+    // Generate the changelog.
+    step('\nGenerating the changelog...')
+    await run('npm', ['run', 'changelog'])
+  
+    const { yes: changelogOk } = await enquirer.prompt({
+      type: 'confirm',
+      name: 'yes',
+      message: `Changelog generated. Does it look good?`
+    })
+  
+    if (!changelogOk) {
+      return
+    }
+  
+    // // Commit changes to the Git.
+    step('\nCommitting changes...')
+    await run('git', ['add', '-A'])
+    await run('git', ['commit', '-m', `release: v${targetVersion}`])
+  
+    // // Publish the package.
+    // step('\nPublishing the package...')
+    // await run ('yarn', [
+    //   'publish', '--tag', tag, '--new-version', targetVersion, '--no-commit-hooks',
+    //   '--no-git-tag-version'
+    // ])
+  
+    // // Push to GitHub.
+    // step('\nPushing to GitHub...')
+    // await run('git', ['tag', `v${targetVersion}`])
+    // await run('git', ['push', 'origin', `refs/tags/v${targetVersion}`])
+    // await run('git', ['push'])
+  }
+  
+  function updatePackage(version) {
+    const pkg = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'))
+  
+    pkg.version = version
+  
+    fs.writeFileSync(packageJsonPath, JSON.stringify(pkg, null, 2) + '\n')
+  }
+  
+  main().catch((err) => console.error(err))
+  
+  // 这段代码实现了一个自动化的包发布流程，主要功能包括：
+  
+  // 提供一个命令行交互式界面，用于选择发布版本类型、自定义版本号、选择发布tag等；
+  // 运行测试、生成changelog、更新版本号、构建包、提交代码到Git、发布包到NPM，以及相关的Git操作，例如打tag、推送到远程仓库等。
+  // 代码分为两部分：
+  
+  // 引入了一些第三方依赖和常量，如fs、path、chalk、semver、prompt、execa和currentVersion等。其中semver用于处理版本号，execa用于在命令行中执行shell命令，prompt用于提供命令行交互式界面。
+  // 定义了几个函数和一个async函数main。其中inc函数用于增加版本号，bin函数用于生成可执行文件路径，run函数用于在命令行中执行shell命令并将结果输出到控制台，step函数用于输出步骤提示信息。updatePackage函数用于更新package.json文件中的版本号，main函数是主要的逻辑，它实现了整个发布流程的自动化。
+  // 整个流程的逻辑如下：
+  
+  // 用户选择发布版本类型，可以选择patch、minor或major，或者选择custom自定义版本号；
+  // 如果用户选择了custom，则需要输入自定义的版本号；
+  // 判断输入的版本号是否合法；
+  // 用户选择发布tag类型，可以选择latest或next；
+  // 确认发布版本号和tag类型是否正确；
+  // 运行测试、更新版本号、构建包、生成changelog等操作；
+  // 确认生成的changelog是否正确；
+  // 提交代码到Git，并打上tag；
+  // 将代码推送到远程仓库；
+  // 发布包到NPM。
